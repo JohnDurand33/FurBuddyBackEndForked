0a430b06b2b6c75f957647488c47dc0a2ad12f42
IrynaPiatnochka <ivpiatnochka@gmail.com>
2024-09-20 19:10:46 -0500

    added color_id/String

--- models/event.py
+++ models/event.py
@@ -1,19 +1,10 @@
 from database import db
 from sqlalchemy import Index
-from enum import Enum
-from sqlalchemy import Enum as SqlEnum
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy.orm import relationship
 from sqlalchemy import Integer, ForeignKey, String, DateTime
 
-
-class Color(Enum):
-    RED = '#FF5733'
-    GREEN = '#28A745'
-    BLUE = '#007BFF'
-    YELLOW = '#FFC107'
-    ORANGE = '#FF6F20'
 
 class Event(db.Model):
     __tablename__ = 'Event'
@@ -26,7 +17,7 @@ class Event(db.Model):
     start_time = db.Column(db.DateTime, nullable=False)
     end_time = db.Column(db.DateTime, nullable=False)
     notes = db.Column(db.Text, nullable=True)
-    color_id = db.Column(SqlEnum(Color), nullable=True)
+    color_id = db.Column(db.String(2), nullable=True) 
     owner_id = db.Column(db.Integer, db.ForeignKey('Owner.id'), nullable=False)  
 
     owner = db.relationship('DogOwner', backref=db.backref('events', lazy=True))
